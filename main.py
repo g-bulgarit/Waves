0984b1eb4aa91b279e2549947f91ba2f953628cb
g-bulgarit <alonio6@gmail.com>
2020-05-27 19:53:46 +0300

    Slight refactor to the code

--- main.py
+++ main.py
@@ -25,93 +25,75 @@ def collapse_pixels_to_line(img_matrix, start_y, end_y):
 
 def generate_sine(darkness_map, frequency_scale=100, vary_amplitude=False):
     omega_arr = darkness_map * 2 * np.pi * frequency_scale
-    amp_scaler = 1
+    amp_scaler = 0.5
 
     if vary_amplitude:
         amp_scaler = darkness_map * 2 * np.pi * 1
         max_amp = np.max(amp_scaler)
-        amp_scaler /= max_amp*1.2
+        amp_scaler /= max_amp
 
     x_arr = np.arange(darkness_map.shape[0])/300
-    sine_points = amp_scaler * np.sin(omega_arr * x_arr)
+    sine_points = 0.5 * amp_scaler * np.sin(omega_arr * x_arr)
     # max_val = np.max(sine_points)
     # sine_normalized = sine_points / max_val
 
     return sine_points
 
 
-def draw_sines(sine_amt, img_mat):
+def draw_sines(sine_amt, img_matrix):
+    # Create an empty image
+    out_size = (img_matrix.shape[1], img_matrix.shape[0])
+    base = Image.new("L", out_size, color=255)
 
+    # Create canvas to draw on
+    canvas = ImageDraw.Draw(base)
+
+    # Calculate the distance between each sine
+    delta = (out_size[0]//sine_amt)
+    x_axis_steps = np.arange(0, out_size[1])
+    # Make an empty points list
     pts_list = []
 
-    # Create an empty image
-    new_image_x = img_mat.shape[1]
-    new_image_y = img_mat.shape[0]
-
-    out_size = (new_image_x, new_image_y)
-
-    base = Image.new("L", out_size, color=255)
-    canvas = ImageDraw.Draw(base)
-    delta = (out_size[0]//sine_amt)
-
-    sample_rate = np.arange(0, out_size[1])
-
-    for current_sine in range(sine_amt-1):
+    # For each sine:
+    for current_sine in range(sine_amt):
+        # Calculate frequency and amplitude:
         dark_map = collapse_pixels_to_line(img_mat,
                                            current_sine * delta,
                                            (current_sine+1) * delta)
-        sine_to_draw = generate_sine(dark_map, vary_amplitude=True)
-        scaled_sine = sine_to_draw*delta
 
-        # DRAW:
+        # Generate the points
+        sine_to_draw = generate_sine(dark_map, vary_amplitude=True)
+        # Scale the sine to the required size on the y-axis.
+        scaled_sine = sine_to_draw*delta
+        # Find the center point for the wave to begin, on the y-axis:
         center_y = int(current_sine*delta) + delta/2
-        for pt in range(0, len(sample_rate)):
-            y_val = center_y + scaled_sine[pt]
-            if np.isnan(y_val):
-                print("nan")
-            else:
-                coord = (pt, int(y_val))
-                pts_list.append(coord)
+
+        # For "x" pixel, find the corresponding y value from the center:
+        for x_val in range(0, len(x_axis_steps)):
+            y_val = center_y + scaled_sine[x_val]
+            point = (x_val, int(y_val))
+            pts_list.append(point)
+
+        # Patchwork, can't be arsed to do the math
         pts_list.append("BREAK")
 
-
-
+    # Draw a line between each two consecutive points
     loc = 1
     while loc < len(pts_list)-2:
         if pts_list[loc] == "BREAK":
+            # Jump a row if we hit the break point
             loc += 3
         xy = pts_list[loc-1], pts_list[loc]
-        canvas.line(xy, fill=15, width=1)
-        loc +=1
-
-    base.show()
-
+        canvas.line(xy, fill=25, width=1)
+        loc += 1
+    return base
 
 
 if __name__ == "__main__":
-    #
-    # Load image
-    # Get parameters from configuration:
-    #   - how many waves in the image
-    #       - this equals the interval (in pixels) where we can center the starts of waves
-    #   - ratio of frequency/darkness
-    #   - change amplitude?
-    # Draw sines with respect to pixel size, changing in frequency
-
     # Configuration
-    total_amt_sines = 100
+    total_amt_sines = 70
 
     # End of Configuration
-
-
     img_file, img_mat = load_image("mari.jpg")
-
-    draw_sines(total_amt_sines, img_mat)
-    # darkness = np.linspace(0, 255, num=2000, dtype=np.float)
-    # collapse_pixels_to_line(img_mat, 0, 5)
-    # q = generate_sine(darkness, vary_amplitude=True)
-
-    # plt.plot(darkness, q)
-    # plt.show()
-
-    pass
+    output = draw_sines(total_amt_sines, img_mat)
+    output.show()
