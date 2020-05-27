8d05562da46289cf2c0c01918215b7bbdba139c1
g-bulgarit <alonio6@gmail.com>
2020-05-27 19:41:17 +0300

    started normalizing values

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from PIL import Image, ImageDraw
 import numpy as np
+from scipy.signal import savgol_filter
 from matplotlib import pyplot as plt
 
 
 def load_image(path):
     img = Image.open(path)
-    img_x = img.size[0]*4
-    img_y = img.size[1]*4
+    img_x = img.size[0]*8
+    img_y = img.size[1]*8
     img = img.resize((img_x, img_y))
     img = img.convert("L")  # move to gray scale
     return img, np.asarray(img)
@@ -15,23 +16,28 @@ def load_image(path):
 def collapse_pixels_to_line(img_matrix, start_y, end_y):
     w_matrix = np.copy(img_matrix)
     line = 255 - (w_matrix[start_y:end_y].sum(axis=0)//(end_y - start_y))
-    line[line < 5] = 3
+    line = savgol_filter(line, 7, 3)  # window size 51, polynomial order 3
+
+    # normalize 0-> 1
+    line = line/255
     return line
 
 
-def generate_sine(darkness_map, frequency_scale=10, vary_amplitude=False):
+def generate_sine(darkness_map, frequency_scale=100, vary_amplitude=False):
     omega_arr = darkness_map * 2 * np.pi * frequency_scale
     amp_scaler = 1
 
     if vary_amplitude:
         amp_scaler = darkness_map * 2 * np.pi * 1
+        max_amp = np.max(amp_scaler)
+        amp_scaler /= max_amp*1.2
 
-    x_arr = np.arange(darkness_map.shape[0])/20000
-    sine_points = amp_scaler*np.sin(omega_arr * x_arr)
-    max_val = np.max(sine_points)
-    sine_normalized = sine_points / max_val
+    x_arr = np.arange(darkness_map.shape[0])/300
+    sine_points = amp_scaler * np.sin(omega_arr * x_arr)
+    # max_val = np.max(sine_points)
+    # sine_normalized = sine_points / max_val
 
-    return sine_normalized
+    return sine_points
 
 
 def draw_sines(sine_amt, img_mat):
@@ -39,8 +45,8 @@ def draw_sines(sine_amt, img_mat):
     pts_list = []
 
     # Create an empty image
-    new_image_x = img_mat.shape[0]
-    new_image_y = img_mat.shape[1]
+    new_image_x = img_mat.shape[1]
+    new_image_y = img_mat.shape[0]
 
     out_size = (new_image_x, new_image_y)
 
@@ -75,7 +81,7 @@ def draw_sines(sine_amt, img_mat):
         if pts_list[loc] == "BREAK":
             loc += 3
         xy = pts_list[loc-1], pts_list[loc]
-        canvas.line(xy, fill=0, width=1)
+        canvas.line(xy, fill=15, width=1)
         loc +=1
 
     base.show()
